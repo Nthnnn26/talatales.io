b3305321263340e32812c687459750b5465bf03e
Nathannn <144454077+Nthnnn26@users.noreply.github.com>
2024-03-12 10:55:20 +0800

    Add files via upload

---
+++ script/tortoise&haregame.js
@@ -0,0 +1,201 @@
+let currentPlayingAudio = null;
+
+function stopCurrentAudio() {
+    if (currentPlayingAudio) {
+        currentPlayingAudio.pause();
+        currentPlayingAudio.currentTime = 0;
+    }
+}
+
+const slow = document.getElementById('audio_slow');
+const laugh = document.getElementById('audio_laugh');
+const run = document.getElementById('audio_run');
+const amused = document.getElementById('audio_amused');
+const consented = document.getElementById('audio_consented');
+const sight = document.getElementById('audio_sight');
+const beside = document.getElementById('audio_beside');
+const sleeping = document.getElementById('audio_sleeping');
+const steadily = document.getElementById('audio_steadily');
+const goal = document.getElementById('audio_goal');
+const swiftest = document.getElementById('audio_swiftest');
+
+const slowBtn = document.getElementById('slow-btn');
+const laughBtn = document.getElementById('laugh-btn');
+const runBtn = document.getElementById('run-btn');
+const amusedBtn = document.getElementById('amused-btn');
+const consentedBtn = document.getElementById('consented-btn');
+const sightBtn = document.getElementById('sight-btn');
+const besideBtn = document.getElementById('beside-btn');
+const sleepingBtn = document.getElementById('sleeping-btn');
+const steadilyBtn = document.getElementById('steadily-btn');
+const goalBtn = document.getElementById('goal-btn');
+const swiftestBtn = document.getElementById('swiftest-btn');
+
+slowBtn.addEventListener('click', () => { slow.play(); });
+laughBtn.addEventListener('click', () => { laugh.play(); });
+runBtn.addEventListener('click', () => { run.play(); });
+amusedBtn.addEventListener('click', () => { amused.play(); });
+consentedBtn.addEventListener('click', () => { consented.play(); });
+sightBtn.addEventListener('click', () => { sight.play(); });
+besideBtn.addEventListener('click', () => { beside.play(); });
+sleepingBtn.addEventListener('click', () => { sleeping.play(); });
+steadilyBtn.addEventListener('click', () => { steadily.play(); });
+goalBtn.addEventListener('click', () => { goal.play(); });
+swiftestBtn.addEventListener('click', () => { swiftest.play(); });
+
+const slowPhonetics = document.getElementById('slow_phonetics');
+const laughPhonetics = document.getElementById('laugh_phonetics');
+const runPhonetics = document.getElementById('run_phonetics');
+const amusedPhonetics = document.getElementById('amused_phonetics');
+const consentedPhonetics = document.getElementById('consented_phonetics');
+const sightPhonetics = document.getElementById('sight_phonetics');
+const besidePhonetics = document.getElementById('beside_phonetics');
+const sleepingPhonetics = document.getElementById('sleeping_phonetics');
+const steadilyPhonetics = document.getElementById('steadily_phonetics');
+const goalPhonetics = document.getElementById('goal_phonetics');
+const swiftestPhonetics = document.getElementById('swiftest_phonetics');
+
+const proSlowBtn = document.getElementById('pro_slow');
+const proLaughBtn = document.getElementById('pro_laugh');
+const proRunBtn = document.getElementById('pro_run');
+const proAmusedBtn = document.getElementById('pro_amused');
+const proConsentedBtn = document.getElementById('pro_consented');
+const proSightBtn = document.getElementById('pro_sight');
+const proBesideBtn = document.getElementById('pro_beside');
+const proSleepingBtn = document.getElementById('pro_sleeping');
+const proSteadilyBtn = document.getElementById('pro_steadily');
+const proGoalBtn = document.getElementById('pro_goal');
+const proSwiftestBtn = document.getElementById('pro_swiftest');
+
+proSlowBtn.addEventListener('click', () => { slowPhonetics.play(); });
+proLaughBtn.addEventListener('click', () => { laughPhonetics.play(); });
+proRunBtn.addEventListener('click', () => { runPhonetics.play(); });
+proAmusedBtn.addEventListener('click', () => { amusedPhonetics.play(); });
+proConsentedBtn.addEventListener('click', () => { consentedPhonetics.play(); });
+proSightBtn.addEventListener('click', () => { sightPhonetics.play(); });
+proBesideBtn.addEventListener('click', () => { besidePhonetics.play(); });
+proSleepingBtn.addEventListener('click', () => { stopCurrentAudio(); currentPlayingAudio = sleepingPhonetics; sleepingPhonetics.play(); });
+proSteadilyBtn.addEventListener('click', () => { stopCurrentAudio(); currentPlayingAudio = steadilyPhonetics; steadilyPhonetics.play(); });
+proGoalBtn.addEventListener('click', () => { goalPhonetics.play(); });
+proSwiftestBtn.addEventListener('click', () => { swiftestPhonetics.play(); });
+
+var gameView = document.querySelector('.game-view');
+var page = 1;
+
+function showGame(gameId) {
+    var game = document.querySelector(gameId);
+    gameView.style.display = 'none';
+    game.style.display = 'block';
+}
+
+window.addEventListener('load', function () {
+var sections = document.querySelectorAll('.story-view');
+
+if (sections.length > 0) {
+    sections[0].style.display = 'block';
+}
+});
+
+function next() {
+page++;
+showQuestion(page);
+}
+
+function showQuestion(page) {
+// Add logic to show the next question based on the page value
+var currentQuestion = document.getElementById('question-' + (page - 1));
+var nextQuestion = document.getElementById('question-' + page);
+
+if (currentQuestion) {
+    currentQuestion.style.display = 'none';
+}
+
+if (nextQuestion) {
+    nextQuestion.style.display = 'block';
+} else {
+    // Handle logic when there are no more questions
+    $('#done').modal('show')
+}
+}
+
+function questionOne(checkAnswer) {
+if (checkAnswer === 1) {
+    $('#correct').modal('show')
+   
+} else {
+    $('#wrong').modal('show')
+   
+}
+}
+
+function questionTwo(checkAnswer) {
+if (checkAnswer === 2) {
+    $('#correct').modal('show')        
+} else {
+    $('#wrong').modal('show')             
+}
+}
+
+function questionThree(checkAnswer) {
+if (checkAnswer === 3) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
+
+function questionFour(checkAnswer) {
+if (checkAnswer === 2) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
+
+function questionFive(checkAnswer) {
+if (checkAnswer === 4) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
+
+function questionSix(checkAnswer) {
+if (checkAnswer === 1) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
+
+function questionSeven(checkAnswer) {
+if (checkAnswer === 3) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
+
+function questionEight(checkAnswer) {
+if (checkAnswer === 2) {
+    $('#correct').modal('show') 
+} else {     
+    $('#wrong').modal('show')  
+}
+}
+
+function questionNine(checkAnswer) {
+if (checkAnswer === 4) {
+    $('#correct').modal('show') 
+} else { 
+    $('#wrong').modal('show')  
+}
+}
+
+function questionTen(checkAnswer) {
+if (checkAnswer === 1) {
+    $('#correct').modal('show') 
+} else {
+    $('#wrong').modal('show')  
+}
+}
---
+++ script/tortoise&hareread.js
@@ -0,0 +1,312 @@
+window.addEventListener('load', function () {
+    var sections = document.querySelectorAll('.story-view');
+
+    if (sections.length > 0) {
+        sections[0].style.display = 'block';
+    }
+});
+
+function toggleVideo(thumbnailId, videoId, subtitleId, textId, control) {
+    var video = document.getElementById(videoId);
+    var subtitle = document.getElementById(subtitleId);
+    var text = document.getElementById(textId);
+    var thumbnail = document.getElementById(thumbnailId);
+    const playPauseBtn = document.getElementById(control);
+
+    text.style.display = "none";
+    video.style.display = "none";
+    thumbnail.style.display = "block";
+    subtitle.style.display = "block";
+              
+    if (subtitle.paused) {
+        subtitle.play();  
+        playPauseBtn.innerHTML = getPauseSVG();
+    } else {
+        subtitle.pause();
+        playPauseBtn.innerHTML = getPlaySVG();
+    }
+    
+    subtitle.addEventListener('ended', function() {
+        thumbnail.style.display = "block";
+        text.style.display = "block";
+        video.style.display = "none";
+        subtitle.style.display = "none";
+        playPauseBtn.innerHTML = getPlaySVG();
+});
+    
+}
+
+function next(currentPage, thumbnailId, videoId, subtitleId, textId, control) {
+    var sections = document.querySelectorAll('.story-view');
+    sections[currentPage - 1].style.display = 'none';
+    var nextPage = currentPage % sections.length + 1;
+    sections[nextPage - 1].style.display = 'block';
+    stopAllVideosAndAudio();
+
+    var video = document.getElementById(videoId);
+    var subtitle = document.getElementById(subtitleId);
+    var text = document.getElementById(textId);
+    var thumbnail = document.getElementById(thumbnailId);
+    const playPauseBtn = document.getElementById(control);
+
+    text.style.display = "block";
+    video.style.display = "block";
+    thumbnail.style.display = "none";
+    subtitle.style.display = "none";
+    playPauseBtn.innerHTML = getPlaySVG();
+}
+
+function previous(currentPage, thumbnailId, videoId, subtitleId, textId, control) {
+    var sections = document.querySelectorAll('.story-view');
+    sections[currentPage - 1].style.display = 'none';
+    var previousPage = (currentPage - 2 + sections.length) % sections.length + 1;
+    sections[previousPage - 1].style.display = 'block';
+    stopAllVideosAndAudio();
+
+    var video = document.getElementById(videoId);
+    var subtitle = document.getElementById(subtitleId);
+    var text = document.getElementById(textId);
+    var thumbnail = document.getElementById(thumbnailId);
+    const playPauseBtn = document.getElementById(control);
+
+    text.style.display = "block";
+    video.style.display = "block";
+    thumbnail.style.display = "none";
+    subtitle.style.display = "none";
+    playPauseBtn.innerHTML = getPlaySVG();
+}
+
+function stopAllVideosAndAudio() {
+    // Stop all videos and reset to 0
+    var allVideos = document.querySelectorAll('video');
+    allVideos.forEach(function(video) {
+        video.pause();
+        video.currentTime = 0;
+      
+    });
+
+    // Stop all audio elements
+    var allAudio = document.querySelectorAll('audio');
+    allAudio.forEach(function(audio) {
+        audio.pause();
+    });
+}
+
+function tipSlow(thumbnailSlow, videoSlow) {          
+var tooltip = document.getElementById('slow');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailslow = document.getElementById(thumbnailSlow);
+var videoslow = document.getElementById(videoSlow);
+
+videoslow.style.display = "block";
+thumbnailslow.style.display = "none";
+videoslow.play();
+
+}
+
+function tipLaugh(thumbnailLaugh, videoLaugh) {
+var tooltip = document.getElementById('laugh');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text1');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnaillaugh = document.getElementById(thumbnailLaugh);
+var videolaugh = document.getElementById(videoLaugh);
+
+videolaugh.style.display = "block";
+thumbnaillaugh.style.display = "none";
+videolaugh.play();
+}
+
+function tipRun(thumbnailRun, videoRun) {
+var tooltip = document.getElementById('run');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text2');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailrun = document.getElementById(thumbnailRun);
+var videorun = document.getElementById(videoRun);
+
+videorun.style.display = "block";
+thumbnailrun.style.display = "none";
+videorun.play();
+}
+
+function tipAmused(thumbnailAmused, videoAmused) {
+var tooltip = document.getElementById('amused');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text3');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailamused = document.getElementById(thumbnailAmused);
+var videoamused = document.getElementById(videoAmused);
+
+videoamused.style.display = "block";
+thumbnailamused.style.display = "none";
+videoamused.play();
+
+}
+
+function tipConsented(thumbnailConsented, videoConsented) {
+var tooltip = document.getElementById('consented');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text4');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailconsented = document.getElementById(thumbnailConsented);
+var videoconsented = document.getElementById(videoConsented);
+
+videoconsented.style.display = "block";
+thumbnailconsented.style.display = "none";
+videoconsented.play();
+}
+
+function tipSight(thumbnailSight, videoSight) {
+var tooltip = document.getElementById('sight');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text5');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailsight = document.getElementById(thumbnailSight);
+var videosight = document.getElementById(videoSight);
+
+videosight.style.display = "block";
+thumbnailsight.style.display = "none";
+videosight.play();
+}
+
+function tipBeside(thumbnailBeside, videoBeside) {
+var tooltip = document.getElementById('beside');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text6');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailbeside = document.getElementById(thumbnailBeside);
+var videobeside = document.getElementById(videoBeside);
+
+videobeside.style.display = "block";
+thumbnailbeside.style.display = "none";
+videobeside.play();
+}
+
+function tipSteadily(thumbnailSteadily, videoSteadily) {
+var tooltip = document.getElementById('steadily');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text7');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailsteadily = document.getElementById(thumbnailSteadily);
+var videosteadily = document.getElementById(videoSteadily);
+
+videosteadily.style.display = "block";
+thumbnailsteadily.style.display = "none";
+videosteadily.play();
+}
+
+function tipSleeping(thumbnailSleeping, videoSleeping) {
+var tooltip = document.getElementById('sleeping');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text8');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailsleeping = document.getElementById(thumbnailSleeping);
+var videosleeping = document.getElementById(videoSleeping);
+
+videosleeping.style.display = "block";
+thumbnailsleeping.style.display = "none";
+videosleeping.play();
+}
+
+function tipGoal() {
+var tooltip = document.getElementById('goal');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text9');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailgoal = document.getElementById(thumbnailGoal);
+var videogoal = document.getElementById(videoGoal);
+
+videogoal.style.display = "block";
+thumbnailgoal.style.display = "none";
+videogoal.play();
+}
+
+function tipSwiftest(thumbnailSwiftest, videoSwiftest) {
+var tooltip = document.getElementById('swiftest');
+tooltip.style.display = 'block';
+var highlightedText = document.querySelector('.highlighted-text10');
+var rect = highlightedText.getBoundingClientRect();
+var centerHorizontal = rect.left + rect.width / 2;
+var tooltipLeft = centerHorizontal - tooltip.offsetWidth / 2;
+tooltip.style.left = tooltipLeft + 'px';
+tooltip.style.top = rect.top - tooltip.offsetHeight + 'px';
+
+var thumbnailswiftest = document.getElementById(thumbnailSwiftest);
+var videoswiftest = document.getElementById(videoSwiftest);
+
+videoswiftest.style.display = "block";
+thumbnailswiftest.style.display = "none";
+videoswiftest.play();
+}
+
+function closeTip(tooltipId) {
+var tooltip = document.getElementById(tooltipId);
+if (tooltip) {
+    tooltip.style.display = 'none';
+}
+}
+
+function getPlaySVG() {
+return `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FCC710" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
+        </svg>`;
+}
+
+function getPauseSVG() {
+return `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="#FCC710" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5"/>
+        </svg>`;
+}
